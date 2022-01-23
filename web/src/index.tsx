94fe1e6fefd5f4d76c4521048189367452f8e353
Connor Turland <connorturland@gmail.com>
2022-01-23 09:36:08 -0800

    fix test suite and set up zome client

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,26 @@
 // Library Imports
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
+import { setupSimpleZomeClient, SimpleZomeClient } from './simpleZomeClient'
 
-// Local Imports
-import { MAIN_APP_ID, PROFILES_SLOT_NAME } from './holochainConfig'
 
 const App: React.FC = () => {
-  return <div>
-    Hello World
-  </div>
+  const zomeClientRef = useRef<SimpleZomeClient>()
+  const [zomeCallResponse, setZomeCallResponse] = useState<number>()
+
+  const init = async () => {
+    const simpleZomeClient = await setupSimpleZomeClient()
+    zomeClientRef.current = simpleZomeClient
+    const result = await simpleZomeClient.callZome<null, number>('first_zome_fn', null)
+    setZomeCallResponse(result)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+  return <div>Hello World, I talked to holochain and got :{zomeCallResponse}</div>
 }
 
 // By passing the `store` in as a wrapper around our React component
 // we make the state available throughout it
-ReactDOM.render(
-    <App />,
-  document.getElementById('react')
-)
+ReactDOM.render(<App />, document.getElementById('react'))
