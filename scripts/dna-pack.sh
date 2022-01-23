fb4b16be3b38981faf99a1dda44dc5ff8737b94c
Connor Turland <connorturland@gmail.com>
2022-01-23 08:52:18 -0800

    fixes, working now

--- scripts/dna-pack.sh
+++ scripts/dna-pack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Compile Rust to WASM
 # add any additional zomes here, in the same way
-cargo build --release --target wasm32-unknown-unknown --manifest-path dna/zomes/zomeA/Cargo.toml
+cargo build --release --target wasm32-unknown-unknown --manifest-path dna/zomes/zome_a/Cargo.toml
 
 # Pack DNAs
 # add any additional dnas here, in the same way
