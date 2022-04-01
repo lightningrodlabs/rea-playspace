fc39e2138ed6a398b6d30cc54327a314afbc7715
Jillian Ada Burrows <jill@adaburrows.com>
2022-03-31 21:01:58 -0700

    Make changes to get the app to run.

---
+++ web/src/holochainConf.ts
@@ -0,0 +1,8 @@
+export const ZOME_NAME = 'zome_a';
+export const REACT_APP_HC_APP_ID = 'hrea_suite';
+// @ts-ignore
+export const MAIN_APP_ID = __MAIN_APP_ID__;
+// @ts-ignore
+export const APP_PORT = __APP_PORT__;
+// @ts-ignore
+export const ADMINPORT = __ADMIN_PORT__;
