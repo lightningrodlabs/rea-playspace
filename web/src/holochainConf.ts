822ea23c5b260e4f85d0b70c52aa197612324af6
Jillian Ada Burrows <jill@adaburrows.com>
2022-04-01 18:27:32 -0700

    Move to vitejs and clean up after ourselves.

--- web/src/holochainConf.ts
+++ web/src/holochainConf.ts
@@ -1,8 +1,8 @@
-export const ZOME_NAME = 'zome_a';
+export const ZOME_NAME = 'projects';
 export const REACT_APP_HC_APP_ID = 'hrea_suite';
 // @ts-ignore
-export const MAIN_APP_ID = __MAIN_APP_ID__;
+export const MAIN_APP_ID = import.meta.env.REA_PLAYSPACE_APP_ID;
 // @ts-ignore
-export const APP_PORT = __APP_PORT__;
+export const APP_PORT = import.meta.env.REA_PLAYSPACE_APP_WS_PORT;
 // @ts-ignore
-export const ADMINPORT = __ADMIN_PORT__;
+export const ADMINPORT = import.meta.env.REA_PLAYSPACE_ADMIN_WS_PORT;
