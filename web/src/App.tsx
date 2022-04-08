845c4df22e122ffa7e472486b97d30eabba96078
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-08 16:57:25 -0400

    first zome call

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,19 +3,17 @@ import "@shoelace-style/shoelace/dist/themes/light.css";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-//import EconomicResourceList from "./EconomicResourceList";
-//import CreateEconomicResource from "./CreateEconomicResource";
 import Header from "./components/Header";
 import LeftScreenNavMenu from "./components/LeftScreenNavMenu";
 //import { SlButton } from "@shoelace-style/shoelace/dist/react";
 import { APP_ID, ZOME_NAME, ADMIN_PORT, APP_PORT } from './holochainConf';
-import { AdminWebsocket, AppWebsocket, HoloHash, CellId } from '@holochain/client';
-import { app } from "electron";
+import { AdminWebsocket, AppWebsocket, InstalledAppInfo, InstalledCell } from '@holochain/client';
 import Knowledge from "./routes/Knowledge";
 import Plan from "./routes/Plan";
 import Observation from "./routes/Observation";
 import Resources from "./components/Resources";
 import NewResource from "./components/NewResource";
+
 const ADMIN_WS_URL = `ws://localhost:${ADMIN_PORT}`;
 const APP_WS_URL = `ws://localhost:${APP_PORT}`;
 
@@ -42,6 +40,10 @@ function StringToHash(s: string): Uint8Array {
 const App: React.FC<Props> = () => {
   const [myAgentId, setMyAgentId] = useState<string>();
   const [client, setClient] = useState<AppWebsocket>();
+  const [result, setResult] = useState("");
+  const [appInfo, setAppInfo] = useState<InstalledAppInfo>();
+  const [cellData, setCellData] = useState<InstalledCell>();
+  const [loading, setLoading] = useState(true);
 
   const connect = async () => {
     const adminWs = await AdminWebsocket.connect(ADMIN_WS_URL);
@@ -55,51 +57,37 @@ const App: React.FC<Props> = () => {
       console.log('app websocket closed');
     })
 
-    if (adminWs.client.socket.readyState === adminWs.client.socket.OPEN
-      && appWs.client.socket.readyState === appWs.client.socket.OPEN) {
-      const appIds = await adminWs.listActiveApps();
-      const infos = await Promise.all(
-        appIds.map(async installed_app_id => {
-          const appInfo = await appWs.appInfo({ installed_app_id });
-          return {
-            ...appInfo,
-            cellIdString: HashToString(appInfo.cell_data[0].cell_id[0]) + ':' + HashToString(appInfo.cell_data[0].cell_id[1])
-          };
-        })
-      );
-      console.log(infos);
-      const cellIds = await adminWs.listCellIds();
-      console.log (cellIds);
-
-      const appDnas = await adminWs.listDnas();
-      console.log(appDnas);
-      
+    // this stinks but works for now.
+    // Check every 100ms and only then get app info or else app_info etc would just get missed.
+    while (!(adminWs.client.socket.readyState === adminWs.client.socket.OPEN
+      && appWs.client.socket.readyState === appWs.client.socket.OPEN)) {
+        continue;
     }
+    const app_info = await appWs.appInfo({ installed_app_id: APP_ID })
+    setAppInfo(app_info)
+    // this always fails on the first load. I need to change and save this file in order for appInfo to be available
+    const cell_data = app_info.cell_data[0]
+    setCellData(cell_data)
     
-    //const zome_res = await client.callZome()
+    // Test 
+    const res = await appWs.callZome({
+      cap_secret: null as any,
+      cell_id: cell_data.cell_id,
+      zome_name: 'projects',
+      fn_name: 'fetch',
+      provenance: cellData.cell_id[1],
+      payload: null as any,
+    }, 30000)
+    setResult(res)
+    setLoading(false)
     
     // @ts-ignore
     setClient(appWs);
-    setMyAgentId('hello');
+    setMyAgentId('hello'); // maybe get rid of this?
   };
 
-  async function connet() {
-
-  }
-
-  // async function fetch() {}
-  //   await wsClient.callZome({
-  //     cap: null,
-  //     cell_id: [1, 2],
-  //     zome_name: "project",
-  //     fn_name: 'fetch',
-  //     provenance: null,
-  //     payload: null,
-  //    }, 30000)
-  // }
-
   useEffect(()=>{
-
+    connect()
   },[])
 
   return (
@@ -107,10 +95,10 @@ const App: React.FC<Props> = () => {
         <div className="container">
           <Header name="shane" />
           <div className="below-header">
-            <LeftScreenNavMenu /*probably pass wsClient here*/ />
+            <LeftScreenNavMenu />
 
             <div className="main-panel">
-              <p>Am I rendering? waiting..."</p>
+              <p>{loading ? "Loading..." : `Fetched From Zome ${result}`}</p>
                 <Routes>
                   <Route
                     path="/knowledge"
