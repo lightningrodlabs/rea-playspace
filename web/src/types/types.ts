61a213dba110e0fb57a855c8bbe8cd69c31e92f7
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-11 09:51:00 -0400

    added rust types and bindings to UI

---
+++ web/src/types/types.ts
@@ -0,0 +1,49 @@
+import { HeaderHashB64 } from "@holochain-open-dev/core-types"
+import { HoloHashB64 } from "@holochain/client"
+
+// container for a set things. Only 1 to begin with
+export interface Project {
+  id: String,
+  name: String
+}
+
+export interface Thing {
+  data: string
+}
+
+// Maps to AddOutput and NewProjectOutput structs
+export interface HoloOutput {
+  header_hash: HoloHashB64,
+  entry_hash: HoloHashB64
+}
+
+export interface UpdateProjectInput {
+  original_header_hash: HeaderHashB64,
+  updated_project: Project
+}
+
+export interface ThingInput {
+  path: String,
+  data: String
+}
+
+export interface Node<T> {
+  idx: number,
+  val: T,
+  parent: number | null,
+  children: number[],
+}
+export interface Tree<T> {
+  tree: Node<T>[]
+}
+
+export interface Content {
+  name: string,
+  data: string,
+}
+
+export enum action {
+  work,
+  accept,
+  use
+}
