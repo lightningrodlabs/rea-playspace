dc074785483721832e4455f2f88e72ce2c863086
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-15 18:12:33 -0400

    serialize path writes, lifted path gets to Home

--- web/src/components/layout/Pallet.tsx
+++ web/src/components/layout/Pallet.tsx
@@ -1,22 +1,21 @@
 import { SlIconButton } from '@shoelace-style/shoelace/dist/react';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import ZomeApi from '../../api/zomeApi';
 import { getZomeApi } from '../../hcWebsockets';
-import { Content, RustNode, Tree } from '../../types/holochain';
 import { Agent, ProcessSpecification, ResourceSpecification } from '../../types/valueflows';
-import { buildTree } from '../../utils';
 import PalletNode from '../PalletNode';
 
-interface Props {}
+interface Props {
+  resourceSpecifications: Array<ResourceSpecification>,
+  processSpecifications: Array<ProcessSpecification>,
+  agents: Array<Agent>
+}
 
-const Pallet: React.FC<Props> = () => {
-
-  const [resources, setResources] = useState<ResourceSpecification[]>([]);
-  const [processes, setProcesses] = useState<ProcessSpecification[]>([]);
-  const [agents, setAgents] = useState<Agent[]>([])
-
-  const zomeApi: ZomeApi = getZomeApi();
+const Pallet: React.FC<Props> = ({
+  resourceSpecifications, 
+  processSpecifications, 
+  agents}) => {
 
   const onDragStart = (event:DragEvent, name: string, type: string) => {
     const data = {name, type};
@@ -24,83 +23,49 @@ const Pallet: React.FC<Props> = () => {
     event.dataTransfer!.effectAllowed = 'move';
   };
 
-  useEffect(()=>{
-    const getResources = async () => {
-      const result: Array<RustNode> = await zomeApi.get_thing('resourceSpecification');
-      const jsTree = buildTree(result, result[0]);
-      const resources = jsTree.children.map((e) => {
-        return JSON.parse(e.val.data) as ResourceSpecification;
-      });
-      setResources(resources);
-    }
-  
-    const getProcesses = async () => {
-      const result = await zomeApi.get_thing('processSpecification');
-      const jsTree = buildTree(result, result[0]);
-      const processes = jsTree.children.map((e) => {
-        return JSON.parse(e.val.data) as ProcessSpecification;
-      });
-      setProcesses(processes);
-    }
+  const resourceSpecificationPalletNodeStyles = {
+      border: "1px solid green",
+      background: "lightgreen",
+      margin: "5px",
+      borderRadius: "10px"
+  }
 
-    const getAgents = async () => {
-      const result = await zomeApi.get_thing('agent');
-      const jsTree = buildTree(result, result[0]);
-      const agents = jsTree.children.map((e) => {
-        return JSON.parse(e.val.data) as Agent;
-      });
-      setAgents(agents);
-    }
-
-    getResources();
-    getProcesses();
-    getAgents();
-  }, []);
-
-const resourcePalletNodeStyles = {
-    border: "1px solid green",
-    background: "lightgreen",
+  const processPalletNodeStyles = {
+    border: "1px solid orange",
+    background: "rgb(255, 213, 128)",
     margin: "5px",
     borderRadius: "10px"
-}
-
-const processPalletNodeStyles = {
-  border: "1px solid orange",
-  background: "rgb(255, 213, 128)",
-  margin: "5px",
-  borderRadius: "10px"
-}
-
-const agentPalletNodeStyles = {
-  border: "1px solid blue",
-  background: "lightblue",
-  margin: "5px",
-  borderRadius: "10px"
-}
-
-function pickStyle(type: string) {
-  if (type === 'resource') return resourcePalletNodeStyles;
-  if (type === 'process') return processPalletNodeStyles;
-  if (type === 'agent') return agentPalletNodeStyles;
-}
-
-function renderNodes(list, type) {
-  if (list.length > 0) {
-    return (list.map((item: any) => (
-      <div 
-      onDragStart={(event: any) => onDragStart(event, item.name, type)} 
-      draggable
-      style={pickStyle(type)}>
-        <PalletNode
-          key={item.id}
-          thing={item}
-        />
-      </div>
-    )));
   }
-  return (<p style={{textAlign: "center"}}>No items</p>);
-}
 
+  const agentPalletNodeStyles = {
+    border: "1px solid blue",
+    background: "lightblue",
+    margin: "5px",
+    borderRadius: "10px"
+  }
+
+  function pickStyle(type: string) {
+    if (type === 'resourceSpecification') return resourceSpecificationPalletNodeStyles;
+    if (type === 'process') return processPalletNodeStyles;
+    if (type === 'agent') return agentPalletNodeStyles;
+  }
+
+  function renderNodes(list, type) {
+    if (list.length > 0) {
+      return (list.map((item: any) => (
+        <div 
+        onDragStart={(event: any) => onDragStart(event, item.name, type)} 
+        draggable
+        style={pickStyle(type)}>
+          <PalletNode
+            key={item.id}
+            thing={item}
+          />
+        </div>
+      )));
+    }
+    return (<p style={{textAlign: "center"}}>No items</p>);
+  }
 
   const palletStyles = {
     flexGrow:1,
@@ -113,17 +78,16 @@ function renderNodes(list, type) {
     padding: "5px"
   }
 
-
   return (
     <aside style={palletStyles}>
       <div style={categoryStyles}>
-      <h2>Resources
+      <h2>Resource Specifications
         <Link to="/resources/new">
           <SlIconButton name="plus-square-fill" label="Settings" />
         </Link>
       </h2>
       </div>
-      {renderNodes(resources, 'resource')}
+      {renderNodes(resourceSpecifications, 'resourceSpecification')}
       <br/>
       <div style={categoryStyles}>
         <h2>Processes
@@ -132,7 +96,7 @@ function renderNodes(list, type) {
         </Link>
         </h2>
       </div>
-      {renderNodes(processes, 'process')}
+      {renderNodes(processSpecifications, 'process')}
       <div style={categoryStyles}>
         <h2>Agents
         <Link to="/agents/new">
