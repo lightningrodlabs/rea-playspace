1c8f50f4f2f51d8e9f7dd86977d00ce9036d8948
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-15 18:11:26 -0400

    better websockets

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { getAgentPubKey, getAppWs, getZomeApi, setAgentPubKey, setCellId, setZomeApi } from './hcWebsockets';
-import { sleep100 } from './utils';
+import { getAppWs, setAgentPubKey, setCellId, setZomeApi } from './hcWebsockets';
 import { APP_ID } from './holochainConf';
 import ZomeApi from './api/zomeApi';
 
 getAppWs().then(async(appWs)=> {
-  appWs.client.socket.addEventListener('close', () => {
-    console.log('app websocket closed');
-  });
-
   const app_info = await appWs.appInfo({ installed_app_id: APP_ID });
-
   const [_dnaHash, agentPubKey] = app_info.cell_data[0].cell_id;
   setAgentPubKey(agentPubKey);
   setCellId(app_info.cell_data[0].cell_id);
