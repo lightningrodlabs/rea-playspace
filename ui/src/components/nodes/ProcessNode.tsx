5481a37c11b1a0af0a68b0068342c13e2a3af42d
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-18 09:27:11 -0400

    process node styles wip

--- ui/src/components/nodes/ProcessNode.tsx
+++ ui/src/components/nodes/ProcessNode.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { CSSProperties } from 'react';
 
 import { Handle, Position } from 'react-flow-renderer';
 
@@ -14,29 +14,72 @@ interface Props {
 const ProcessNode: React.FC<Props> = ({data}) => {
 
   const nodeStyles = {
-    border: "1px solid orange",
+    border: '1px solid orange',
+    width: '125px',
+    height: '50px',
     background: "rgb(255, 213, 128)",
-    margin: "5px",
     borderRadius: "10px"
   }
+
+  const nodeHeader: CSSProperties = {
+    borderBottomWidth: '1px', 
+    borderBottomStyle: 'solid',
+    fontSize: '10px',
+    paddingLeft: '8px',
+    paddingTop: '4px',
+    paddingBottom: '4px', 
+    borderColor: 'orange'
+  }
+
+  const handleRight: CSSProperties = {
+    width: "8px", 
+    height: '8px', 
+    backgroundColor: 'rgb(189, 196, 204)', 
+    border: 'orange solid 1px', 
+    position:'absolute', 
+    right: '-4px',
+    top: '35px'
+  }
+
+  const handleLeft: CSSProperties = { 
+    width: "8px", 
+    height: '8px', 
+    backgroundColor: 'rgb(189, 196, 204)', 
+    border: 'white solid 1px', 
+    position:'absolute', 
+    left: '-4px',
+    top: '35px'
+  }
+
+  const nodeBody: CSSProperties = {
+    fontSize: '10px',
+    paddingLeft: "12px", 
+    paddingRight: "12px"
+  }
+
   return (
-    <>
-      <Handle
-        type="target"
-        position={Position.Left}
-        style={{ background: '#555' }}
-        onConnect={(params) => console.log('handle onConnect', params)}
-      />
+    <div style={{position:'relative', overflow: 'hidden'}}>
+
       <div style={nodeStyles}>
-        <p style={{paddingLeft: "2px", paddingRight: "2px"}}><strong>{data.name}</strong></p>
+        <div style={nodeHeader}>{data.label}</div>
+        <div style={nodeBody}>
+          <Handle
+            type="target"
+            position={Position.Left}
+            style={handleLeft}
+            onConnect={(params) => console.log('handle onConnect', params)}
+          />
+          <p>{data.name}</p>
+          <Handle
+            type="source"
+            position={Position.Right}
+            id="a"
+            style={handleRight}
+          />
+        </div>
       </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id="a"
-        style={{ background: '#555' }}
-      />
-    </>
+
+    </div>
   );
 }
 
