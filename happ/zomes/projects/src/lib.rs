c35d7e9d0ca72d28b571480c4d6a38584939b71c
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-27 11:50:21 -0400

    reindexing logic

--- happ/zomes/projects/src/lib.rs
+++ happ/zomes/projects/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use hdk::prelude::*;
 use holo_hash::{EntryHashB64, HeaderHashB64};
 use tracing::{info};
@@ -97,26 +99,11 @@ fn build_tree(tree: &mut Tree<Content>, node: usize, path: Path) -> ExternResult
   Ok(())
 }
 
-// fn _update_parent(tree: &mut Tree<Content>, parent_idx:usize, current_idx: usize) -> ExternResult<()> {
-//   // update children Vec of parent
-//   info!("Updating Parent...");
-//   match tree.tree.clone().into_iter().find(|node| node.idx == parent_idx) {
-//     Some(mut node) => {
-//       info!("updating children: {:?}", node.children);
-//       node.children.retain(|&x| x != current_idx);
-//       info!("updated children: {:?}", node.children);
-//     },
-//     None => info!("Nothing at parent_idx.."),
-//   }
-//   Ok(())
-// }
-
-// Dead node tagger
+// Dead node marker
 fn mark_tree_for_delete(tree: &mut Tree<Content>, to_delete: &mut Vec<bool>) -> ExternResult<()>{
   for node in tree.tree.clone().into_iter().rev() {
     if node.children.len() == 0 && node.val.data.eq("") {
       to_delete[node.idx] = true;
-      info!("Adding node: {}", node.idx);
     }
 
     let mut are_children_deleted: bool =  true;
@@ -126,12 +113,12 @@ fn mark_tree_for_delete(tree: &mut Tree<Content>, to_delete: &mut Vec<bool>) -> 
 
     if are_children_deleted && node.val.data.eq("") && node.val.name != "root" {
       to_delete[node.idx] = true;
-      info!("Adding node: {}", node.idx);
     }
   };
   Ok(())
 }
 
+// removed marked nodes
 fn prune_tree(tree: &mut Tree<Content>, pruned_tree: &mut Tree<Content>, to_delete: &mut Vec<bool>) -> ExternResult<()> {
 
    // convert Vec<bool> to Vec<usize>
@@ -155,9 +142,60 @@ fn prune_tree(tree: &mut Tree<Content>, pruned_tree: &mut Tree<Content>, to_dele
   Ok(())
 }
 
+// update idx, parents and children
+fn reindex_tree(pruned_tree: Tree<Content>, reindexed_tree: &mut Tree<Content>) -> ExternResult<()> {
+  info!("Reindexing tree...");
+  // K: old idx of node V: new idx of node
+  // This map is populated as nodes get reindexed.
+  // This map get used to update the children Vec of nodes.
+  let mut idx_map: BTreeMap<usize, usize> = BTreeMap::new();
+
+  let rev_index: Vec<usize> = (0..pruned_tree.tree.len()).rev().collect();
+  let it = rev_index.iter().zip(pruned_tree.tree.iter().rev());
+  // crawl through nodes backwards and get map of old and new idxs
+  for (_, (i, node)) in it.enumerate() {
+    // nodes idx and location in tree Vec are mismatched. Condition to reindex.
+    if *i != node.idx {
+      // insert map of index change
+      idx_map.insert(node.idx, *i);
+    }
+  }
+
+  // for each node, update the parent and children from mapped idxs
+  // push onto reindexed_tree
+  for node in pruned_tree.tree.iter() {
+    let mut reindexed_node = node.clone();
+
+    for entry in idx_map.iter() {
+
+      // update idx
+      if node.idx == *entry.0 {
+        reindexed_node.idx = *entry.1;
+      }
+
+      // update children
+      for child in node.children.iter() {
+        if child == entry.0 {
+          reindexed_node.children.retain(|c| c != entry.0);
+          reindexed_node.children.push(*entry.1);
+        }
+      }
+
+      // update parent
+      if node.parent == Some(*entry.0) {
+        reindexed_node.parent = Some(*entry.1);
+      }
+    }
+    reindexed_tree.tree.push(reindexed_node);
+  }
+  
+  reindexed_tree.tree.sort_by(|a, b| a.idx.cmp(&b.idx));
+  info!("Reindexing tree complete.");
+  Ok(())
+}
+
 #[hdk_extern]
 pub fn get_thing(path_str: String) -> ExternResult<Option<Tree<Content>>> {
-  info!("getting thing with path: '{}'", path_str.clone());
   let root_path = Path::from(path_str.clone());
 
   let val = Content {
@@ -176,8 +214,11 @@ pub fn get_thing(path_str: String) -> ExternResult<Option<Tree<Content>>> {
   
   let mut pruned_tree: Tree<Content> = Tree { tree: vec![] };
   prune_tree(&mut tree, &mut pruned_tree, &mut to_delete)?;
- 
-  Ok(Some(pruned_tree))
+
+  let mut reindexed_tree: Tree<Content> = Tree { tree: vec![] };
+  reindex_tree(pruned_tree, &mut reindexed_tree)?;
+
+  Ok(Some(reindexed_tree))
 }
 
 #[hdk_extern]
