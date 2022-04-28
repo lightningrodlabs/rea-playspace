4fabd76a5bdb4f6a7220224328b7728d3a71e574
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-04-28 13:39:45 -0400

    remove projects from tests

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,7 +1,6 @@
 
 import { Orchestrator } from "@holochain/tryorama";
 
-//import projects_project from './rea_playspace/projects/project';
 import tree from './rea_playspace/tree/tree';
 
 let orchestrator: Orchestrator<any>;
