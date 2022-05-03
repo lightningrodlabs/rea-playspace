70e893a28d5fe444cd9a83b6fbcd1e01e16318e5
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-05-03 13:56:09 -0400

    Added nickname to Header

--- ui/src/components/ProfilePrompt.tsx
+++ ui/src/components/ProfilePrompt.tsx
@@ -2,49 +2,52 @@ import React, { useEffect, useState } from "react";
 import {
   CreateProfile
 } from "../elements";
-import { getProfilesService, getProfilesStore } from "../data/ProfilesStore";
-import { AgentProfile, Profile, ProfilesService, ProfilesStore } from "@holochain-open-dev/profiles";
-
-import {
-  Button,
-  CircularProgress,
-  TextField,
-} from '@scoped-elements/material-web';
+import { getMyProfile, getProfilesService, setMyProfile } from "../data/ProfilesStore";
+import { ProfilesService } from "@holochain-open-dev/profiles";
 
 interface Props {}
 
+/**
+ * 
+ * This is a workaround because ProfilePrompt doesn't work with React out 
+ * of the box.
+ */
 const ProfilePrompt: React.FC<Props> = ({children}) => {
-  const [myProfile, setMyProfile] = useState<AgentProfile>();
+  const [profileExists, setProfileExists] = useState<Boolean>(false);
   let service: ProfilesService = getProfilesService();
 
-  window.addEventListener('profile-created', (event) => {
+  function handleCreateProfile() {
     service.getMyProfile().then((profile) => {
-      console.log('profile to set: ', profile);
-      setMyProfile(profile);
+      if (profile) {
+        setMyProfile(profile);
+        // I don't want this. Right now it's just to trigger a re-render
+        setProfileExists(true);
+      }
     });
-  });
-
+  }
+  window.addEventListener('profile-created', handleCreateProfile);
   useEffect(() => {
     service.getMyProfile().then((profile) => {
-      console.log('profile to set: ', profile);
       setMyProfile(profile);
+      if (profile) {
+        setProfileExists(true);
+      }
     });
   }, []);
 
-  console.log("profile: ", myProfile)
-
   const RenderPrompt = () => {
-    if (myProfile == null) {
+    if (getMyProfile() == null) {
       return (
         <div
         style={{"display": "flex", "flexDirection": "column", "alignItems": "center", "justifyContent": "center", "flex": "1"}}>
           <div style={{"display": "flex", "flexDirection": "column", "alignItems": "center"}}>
-            <slot name="hero"></slot>
             <CreateProfile></CreateProfile>
           </div>
         </div>
       );
     } else {
+      // children is the rest of the app,  <Main />. Can this 
+      // be improved?
       return(
         <slot>{children}</slot>
       )
