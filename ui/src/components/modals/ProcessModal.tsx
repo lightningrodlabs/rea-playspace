bb57dc6aa44100029e5b9deebd21147c1e07e7d2
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-05-18 11:34:40 -0400

    improved process modal and agents not draggable

--- ui/src/components/modals/ProcessModal.tsx
+++ ui/src/components/modals/ProcessModal.tsx
@@ -1,5 +1,5 @@
 import { AgentProfile } from '@holochain-open-dev/profiles';
-import { SlButton, SlDropdown, SlCheckbox, SlDivider, SlIcon, SlInput, SlMenu, SlMenuItem, SlTextarea } from '@shoelace-style/shoelace/dist/react';
+import { SlButton, SlDropdown, SlCheckbox, SlDivider, SlIcon, SlInput, SlMenu, SlMenuItem, SlTextarea, SlSelect } from '@shoelace-style/shoelace/dist/react';
 import { Guid } from 'guid-typescript';
 import React, { useEffect, useRef, useState } from 'react';
 import { XYPosition } from 'react-flow-renderer';
@@ -7,14 +7,7 @@ import getDataStore from '../../data/DataStore';
 import { PathedData } from '../../data/models/PathedData';
 import { Agent, ProcessSpecification } from '../../data/models/Valueflows/Knowledge';
 import { Process } from "../../data/models/Valueflows/Plan";
-import { getProfilesService } from '../../data/ProfilesStore';
-
-const useConstructor = (callBack = () => {}) => {
-  const hasBeenCalled = useRef(false);
-  if (hasBeenCalled.current) return;
-  callBack();
-  hasBeenCalled.current = true;
-}
+import { AgentShape } from '../../types/valueflows';
 
 const initialState = {
   name: '',
@@ -37,26 +30,24 @@ const ProcessModal: React.FC<Props> = ({
   handleAddNode
 }) => {
   const [
-    {name, finished, note, classifiedAs, inScopeOf, basedOn}, setState
+    {name, 
+    finished, 
+    note, 
+    classifiedAs, 
+    inScopeOf, 
+    basedOn}, setState
   ] = useState(initialState);
-  const [agentMap, setAgentMap] = useState<Map<string, JSX.Element>>(new Map());
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useConstructor(() => {
-    console.log("only once?");
-    fetchAgents().then((agentsMap) => {
-      setAgentMap(agentsMap);
-    });
-  });
+  const [agents, setAgents] = useState<AgentShape[]>([]);
 
   useEffect(()=>{
     let processSpec: ProcessSpecification = getDataStore().getCursor(processSpecificationPath);
-    setState(prevState => ({ ...prevState, name: processSpec.name, note:processSpec.note }));
+    setState(prevState => ({ ...prevState, name: processSpec.name}));
+    if (processSpec.note != null) {
+      setState(prevState => ({ ...prevState, note:processSpec.note}));
+    }
+    const store = getDataStore();
+    setAgents(store.getAgents());
   },[]);
-
-  useEffect(() => {
-    setLoading(false);
-  }, [agentMap]); // Only re-run the effect if agents changes
 
   const clearState = () => {
     setState({ ...initialState });
@@ -65,11 +56,6 @@ const ProcessModal: React.FC<Props> = ({
   const onChange = e => {
     const { name, value } = e.target;
     setState(prevState => ({ ...prevState, [name]: value }));
-  };
-
-  const handleSelect = e => {
-    const agentName = e.detail.item.innerHTML;
-    setState(prevState => ({ ...prevState, inScopeOf: agentName }));
   };
 
   const handleSubmit = async (e) => {
@@ -87,88 +73,61 @@ const ProcessModal: React.FC<Props> = ({
     closeModal();
   }
 
-  async function fetchAgents() {
-    const profiles: AgentProfile[] = await getProfilesService().getAllProfiles();
-    const agents: Agent[] = getDataStore().getAgents();
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <SlInput
+          required
+          label="Name"
+          name="name"
+          // @ts-ignore
+          onSlInput={onChange}
+          value={name}
 
-    const agentList: Array<string> = [];
-    profiles.forEach((profile) => {
-      agentList.push(profile.profile.nickname);
-    });
-    agents.forEach((agent) => {
-      agentList.push(agent.name);
-    });
+        />
+        <br />
+        <SlCheckbox
+          name="finished"
+          // @ts-ignore
+          onSlInput={onChange}
+          checked={finished}
+        >Finished</SlCheckbox> 
+        <br />
+        <br />
 
-    const dropdownList: Map<string, JSX.Element> = new Map(agentList.map((agent) => {
-      return [agent, <SlMenuItem key={Guid.raw()}>{agent}</SlMenuItem>];
-    }));
+        <SlSelect 
+          label="In Scope Of" 
+          name='inScopeOf' 
+          value={inScopeOf} 
+          onSlChange={onChange} 
+          required>
+          {agents.map((agent) => (<SlMenuItem key={`agent_${agent.id}`} value={agent.id}>{agent.name}</SlMenuItem>))}
+        </SlSelect>
 
-    return dropdownList;
-  }
-  
-  function buttonText() {
-    console.log('in scope of: ', inScopeOf);
-    return inScopeOf == '' ? 'In Scope Of' : inScopeOf;
-  }
-
-  if (loading) {
-    return (<p>Loading...</p>);
-  } else {
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <br />
-          <SlInput
-            required
-            label="Name"
-            name="name"
-            // @ts-ignore
-            onSlInput={onChange}
-            value={name}
-
-          />
-          <br />
-          <SlCheckbox
-            name="finished"
-            // @ts-ignore
-            onSlInput={onChange}
-            checked={finished}
-          >Finished</SlCheckbox> 
-          <br />
-          <br />
-          <SlDropdown>
-            <SlButton slot="trigger" caret>
-              {buttonText()}
-            </SlButton>
-            <SlMenu onSlSelect={handleSelect}>
-              {agentMap.values}
-            </SlMenu>
-          </SlDropdown>
-          <br />
-          <br />
-          <SlInput
-            label="Based On"
-            name="basedOn"
-            // @ts-ignore
-            onSlInput={onChange}
-            value={basedOn}
-          />
-          <br />
-          <SlTextarea
-            label='Note'
-            name='note'
-            // @ts-ignore
-            onSlInput={onChange}
-            value={note}
-          />
-          <br />
-          <SlButton type="submit" variant="primary">
-            Create
-          </SlButton>
-        </form>
-      </>
-    );
-  }
+        <br />
+        <SlInput
+          label="Based On"
+          name="basedOn"
+          // @ts-ignore
+          onSlInput={onChange}
+          value={basedOn}
+        />
+        <br />
+        <SlTextarea
+          label='Note'
+          name='note'
+          // @ts-ignore
+          onSlInput={onChange}
+          value={note}
+        />
+        <br />
+        <SlButton type="submit" variant="primary">
+          Create
+        </SlButton>
+      </form>
+    </>
+  );
 }
 
 export default ProcessModal;
