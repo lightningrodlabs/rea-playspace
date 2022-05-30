68c3e334c67dc7c88f58019955633c555965a0b1
Jillian Ada Burrows <jill@adaburrows.com>
2022-05-30 09:12:04 -0700

    add explainer comment on React SynthicEvent pools

--- ui/src/lib/fiber.ts
+++ ui/src/lib/fiber.ts
@@ -25,8 +25,25 @@ export class Fiber {
    * This takes an array of Promises or async functions (thunks) and ensures they
    * are executed in sequential fashion over the lifetime of this fiber.
    *
-   * **Note:** Do not enclose any React state variables or function parameters
-   * without first making a clone of the structures.
+   * **Note:** While using React before v17, do not enclose any React state
+   * variables or function parameters without first making a clone of the
+   * structures or calling SyntheticEvent.persist() if you prefer not to clone.
+   * It prevents the event model from returning the event object to the pool.
+   *
+   * For instance, if you are using the to asynchronously update a DB or
+   * blockchain, you might find the object passed in as the parameter to into the
+   * function has been changed and no longer reflects what you intended to save.
+   * Most likely it will have undefined parameters. This is because in certain
+   * events the specific object is reused and it's properties are cleared after
+   * the event handler is done executing. That leaves you with a reference to an
+   * empty object.
+   *
+   * See [https://reactjs.org/docs/legacy-event-pooling.html] for the official
+   * take on this. This doesn't happen in React v17 and above.
+   *
+   * See [https://medium.com/trabe/react-syntheticevent-reuse-889cd52981b6] for
+   * more in-depth examples and exmplantion of the SyntheticEvent pool object
+   * reuse.
    */
   public schedule(actions: Array<Action>) {
     this.actionFiber = actions.reduce((chain: Promise<void>, curr) => chain.then(curr), this.actionFiber);
