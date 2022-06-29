ec66e6a555fb763ad08f43e043eb3364df8a8dac
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-06-29 13:11:22 -0400

    wip

--- happ/zomes/projects/src/lib.rs
+++ happ/zomes/projects/src/lib.rs
@@ -2,13 +2,12 @@ mod test;
 mod data;
 mod tree_clean;
 
-use hdk::prelude::*;
+use hdk::{prelude::*, hash_path::path::Component};
 use holo_hash::{EntryHashB64, HeaderHashB64};
 use tracing::{info};
 use data::*;
 use tree_clean::{mark_tree, reindex_tree, prune_tree};
 
-// entry defs
 entry_defs![
   PathEntry::entry_def(),
   Thing::entry_def()
@@ -27,7 +26,7 @@ pub fn put_thing(input: ThingInput) -> ExternResult<AddOutput> {
   create_link(
     anchor_hash, 
     entry_hash.clone(),
-    LinkType(0),
+    HdkLinkType::Any,
     LinkTag::new("data"))?;
 
   let output = AddOutput {
@@ -39,6 +38,7 @@ pub fn put_thing(input: ThingInput) -> ExternResult<AddOutput> {
 }
 
 fn get_entry(path: &Path, tag: LinkTag) -> ExternResult<Option<Thing>> {
+  debug!("getting entry...");
   let links = get_links(path.path_entry_hash()?, Some(tag))?;
   match links.into_iter().max_by(|x, y| x.timestamp.cmp(&y.timestamp)) {
     None => Ok(None),
@@ -56,29 +56,80 @@ fn get_entry(path: &Path, tag: LinkTag) -> ExternResult<Option<Thing>> {
   }  
 }
 
-fn build_tree(tree: &mut Tree<Content>, node: usize, path: Path) -> ExternResult<()>{
-  // root.plan.p1-guid.process.pr-guid -> for each segment in path
-  for path in path.children_paths()? {
-    let v = path.as_ref();
+fn children_pathz(path: Path) -> ExternResult<Vec<Path>> {
+  let children = path.children()?;
+  let components: ExternResult<Vec<Option<Component>>> = children
+      .into_iter()
+      .map(|link| {
+          let component_bytes = &link.tag.0[..];
+          warn!("bytes: {:?}", component_bytes.clone());
+          if component_bytes.is_empty() {
+              Ok(None)
+          } else {
+              let vec_bytes = component_bytes.to_vec();
+              warn!("bytes to vec: {:?}", vec_bytes);
+              let unsafe_bytes = UnsafeBytes::from(vec_bytes);
+              warn!("unsafeBytes: {:?}", unsafe_bytes);
+              let serialized_bytes = SerializedBytes::from(unsafe_bytes);
+              warn!("Serialized Bytes: {:?}", serialized_bytes);
+              Ok(Some(
+                serialized_bytes.try_into().map_err(WasmError::Serialize)?      
+              ))
+          }
+      })
+      .collect();
+  warn!("made it here: {:?}", components.clone());
+  Ok(components?
+      .into_iter()
+      .map(|maybe_component| {
+          let mut new_path = path.clone();
+          debug!("new_path: {:?}", new_path.clone());
+          if let Some(component) = maybe_component {
+              debug!("append component");
+              new_path.append_component(component);
+              debug!("appended path: {:?}", new_path.clone());
+          }
+          new_path
+      })
+      .collect()
+    )
+}
 
-    let data = match get_entry(&path, LinkTag::new("data"))? {
-      Some(thing) => thing.data,
+fn build_tree(tree: &mut Tree<Content>, node: usize, path: Path) -> ExternResult<()>{
+  debug!("path: {:?}", path.path_entry()?);
+  match children_pathz(path.clone()) {
+    Ok(path) => debug!("path: {:?}", path),
+    Err(err) => error!("bad path or something: {}", err),
+  }
+
+  debug!("Building tree at node: {}, tree: {:?}, path: {:?}", &node, tree.clone(), path.clone());
+  for child_path in children_paths(path.clone())? {
+    let v: &Vec<Component> = child_path.as_ref();
+
+    let data = match get_entry(&child_path, LinkTag::new("data"))? {
+      Some(thing) => {
+        debug!("got thing: {:?}", thing.clone());
+        thing.data
+      },
       None => "".into()
     };
+    debug!("data: {:?}", data.clone());
     let val = Content {
       name: String::try_from(&v[v.len()-1])?,
       data: data
     };
+    debug!("val in build tree: {:?}", val.clone());
     let idx = tree.insert(node, val);
-    build_tree(tree, idx, path)?;
+    build_tree(tree, idx, child_path)?;
   }
+  debug!("done building tree");
   Ok(())
 }
 
 #[hdk_extern]
 pub fn get_thing(path_str: String) -> ExternResult<Option<Tree<Content>>> {
+  debug!("get thing with path: {}", path_str.clone());
   let root_path = Path::from(path_str.clone());
-
   let val = Content {
       name: String::from(path_str.clone()),
       data: match get_entry(&root_path, LinkTag::new("data"))? {
@@ -86,19 +137,22 @@ pub fn get_thing(path_str: String) -> ExternResult<Option<Tree<Content>>> {
         None => "".into()
       }
   };
+  debug!("val: {:?}", val.clone());
 
   let mut tree = Tree::new(val);
   build_tree(&mut tree, 0, root_path)?;
- 
+  debug!("built tree?");
+  debug!("tree: {:?}", tree.clone());
   let mut to_delete = vec![false; tree.tree.len()];
   mark_tree(&mut tree, &mut to_delete)?;
-  
+  debug!("to_delete: {:?}", to_delete);
   let mut pruned_tree: Tree<Content> = Tree { tree: vec![] };
   prune_tree(&mut tree, &mut pruned_tree, &mut to_delete)?;
-
+  debug!("pruned_tree: {:?}", pruned_tree.clone());
   let mut reindexed_tree: Tree<Content> = Tree { tree: vec![] };
   reindex_tree(pruned_tree, &mut reindexed_tree)?;
-
+  
+  debug!("reindexed_tree: {:?}", reindexed_tree);
   Ok(Some(reindexed_tree))
 }
 
