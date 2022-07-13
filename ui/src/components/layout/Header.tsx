80f3cf3f18b5474e9b095e20cdf3de6605cc6744
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-07-13 11:34:44 -0400

    removed cat header, updated hc client (#87)

--- ui/src/components/layout/Header.tsx
+++ ui/src/components/layout/Header.tsx
@@ -12,16 +12,6 @@ const Header: React.FC<HeaderProps> = () => {
 
   return (
     <div className="header">
-      <SlAvatar
-        image="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"
-        label="Avatar of a gray tabby kitten looking down"
-      />
-      <div className="search">
-        <SlInput placeholder="Search..." clearable>
-          <SlIcon name="search" slot="prefix"></SlIcon>{" "}
-        </SlInput>
-      </div>
-
       <div>
         <SlAvatar shape="circle" label="Circle avatar" />
         {" "}
