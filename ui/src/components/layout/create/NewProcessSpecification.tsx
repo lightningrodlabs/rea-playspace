f0d0e38d8cfdd7a2da577da73b81a1554e8ccc98
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-07-22 15:42:32 -0400

    made /new views scrollable

--- ui/src/components/layout/create/NewProcessSpecification.tsx
+++ ui/src/components/layout/create/NewProcessSpecification.tsx
@@ -49,7 +49,7 @@ const NewProcessSpecification: React.FC<NewProcessSpecificationProps> = () => {
           <SlButton variant="warning">Cancel</SlButton>
         </Link>
       </MainPanelHeader>
-      <SlCard className="create-resource">
+      <SlCard className="create-resource scrollable-view">
       <form onSubmit={handleSubmit}>
       <br />
         <br />
