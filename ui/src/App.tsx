670692f59a9ff40db8153d9cb471b462d6c35f5c
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-08-09 17:47:14 -0400

    wip

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
