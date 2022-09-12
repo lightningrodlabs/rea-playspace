75e7a2247de6fbfda90e55844265ca5a94e641f4
Jillian Ada Burrows <jill@adaburrows.com>
2022-09-11 19:02:53 -0700

    Issue #124: show process name instead of GUID

--- ui/src/components/input/shared.tsx
+++ ui/src/components/input/shared.tsx
@@ -1,14 +1,18 @@
 import { SlInput } from "@shoelace-style/shoelace/dist/react";
+import getDatastore  from "../../data/DataStore";
 
 export const inputOrOutputOf = (inputOf: string, outputOf: string) => {
+  const store = getDatastore();
   if (inputOf) {
+    const input = store.getById(inputOf);
     return (<>
-      <SlInput disabled label="Input of" name="inputOf" value={inputOf}></SlInput>
+      <SlInput disabled label="Input to process" name="inputOf" value={input.name}></SlInput>
       <br />
     </>)
   } else if (outputOf) {
+    const output = store.getById(outputOf);
     return (<>
-      <SlInput disabled label="Output of" name="outputOf" value={outputOf}></SlInput>
+      <SlInput disabled label="Output from process" name="outputOf" value={output.name}></SlInput>
       <br />
     </>)
   } else {
