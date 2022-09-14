46492dd2b36a4f33343ed4ada3b1a7d882a0afb5
Jillian Ada Burrows <jill@adaburrows.com>
2022-09-13 17:26:15 -0700

    Fix # 151: clean up and refactor code around the profile module.

---
+++ ui/src/data/hooks/useStore.ts
@@ -0,0 +1,62 @@
+import { useDebugValue } from 'react';
+import useSyncExternalStoreExports from 'use-sync-external-store/shim';
+import useSyncExternalStoreWithSelectorExports from 'use-sync-external-store/shim/with-selector';
+const { useSyncExternalStore } = useSyncExternalStoreExports;
+const { useSyncExternalStoreWithSelector } = useSyncExternalStoreWithSelectorExports;
+
+/*
+ * This uses the React v18 compatible shim for synchronous updates. Here's a
+ * useful article:
+ *  - https://blog.saeloun.com/2021/12/30/react-18-usesyncexternalstore-api
+ * React documentation:
+ *  - https://reactjs.org/docs/hooks-reference.html#usesyncexternalstore
+ * Also, there is useful conversation on GitHub here:
+ *  - https://github.com/reactwg/react-18/discussions/86
+ */
+
+/**
+ * API Singletons created for ease of interaction with React
+ */
+export interface SyncExternalStoreApi<Snapshot> {
+  subscribe: (_: () => void) => () => void;
+  getSnapshot: () => Snapshot;
+};
+
+/**
+ * useStore React hook:
+ * Takes an adapted store (an object with the interface SyncExternalStoreApi),
+ * and returns the current snapshot for use in the API. It also triggers
+ * rerendering when the data changes.
+ */
+export function useStore<Snapshot>(
+  store: SyncExternalStoreApi<Snapshot>
+): Snapshot {
+  const state = useSyncExternalStore<Snapshot>(
+    store.subscribe,
+    store.getSnapshot,
+    store.getSnapshot
+  )
+  useDebugValue(state);
+  return state;
+}
+
+/**
+ * useStoreWithSelector React hook:
+ * Takes an adapted store (an object with the interface SyncExternalStoreApi) and
+ * a selector that returns a portion of the store to compare against.
+ * and returns the current snapshot for use in the API. It also triggers
+ * rerendering when the data changes.
+ */
+export function useStoreWithSelector<Snapshot, Selection>(
+  store: SyncExternalStoreApi<Snapshot>,
+  selector: (snapshot: Snapshot) => Selection
+): Selection {
+  const state = useSyncExternalStoreWithSelector<Snapshot>(
+    store.subscribe,
+    store.getSnapshot,
+    store.getSnapshot,
+    selector
+  )
+  useDebugValue(state);
+  return state;
+}
--- ui/src/hooks/useStore.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { StoreSubscriber } from "lit-svelte-stores";
-import { useController } from '@lit-labs/react/use-controller.js';
-
-export const useStore = (store) =>
-  useController(React, (host) => new StoreSubscriber(host, () => store));
