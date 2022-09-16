014408994baea2f4d298bcbae36dc8fda7a7ba96
Jillian Ada Burrows <jill@adaburrows.com>
2022-09-16 00:53:07 -0700

    Fix #135: change styles and add new handles for other connections.

--- ui/src/components/nodes/ResourceSpecificationNode.tsx
+++ ui/src/components/nodes/ResourceSpecificationNode.tsx
@@ -16,19 +16,59 @@ const ResourceSpecificationNode: React.FC<Props> = ({data, selected}) => {
   const className = 'canvas-node-base resource-specification-colors' + (selected ? ' resource-specification-selected' : '' );
   return (
     <>
-      <div key={data.id} style={{position:'relative'}}>
+      <div key={data.id}>
       <div className={className}>
           <div className='canvas-node-body'>
-            <Handle
-              type="target"
-              position={Position.Left}
-              className='canvas-node-handle-base canvas-node-handle-left'
-            />
             <p>{data.name}</p>
             <Handle
               type="source"
+              id="left"
+              position={Position.Left}
+              className='canvas-node-handle-base canvas-node-handle-left'
+            />
+            <Handle
+              type="target"
+              id="left"
+              position={Position.Left}
+              className='canvas-node-handle-base canvas-node-handle-left'
+            >
+            </Handle>
+            <Handle
+              type="source"
+              id="top"
+              position={Position.Top}
+              className='canvas-node-handle-base canvas-node-handle-top'
+            />
+            <Handle
+              type="target"
+              id="top"
+              position={Position.Top}
+              className='canvas-node-handle-base canvas-node-handle-top'
+            />
+            <Handle
+              type="source"
+              id="right"
               position={Position.Right}
               className='canvas-node-handle-base canvas-node-handle-right'
+            />
+            <Handle
+              type="target"
+              id="right"
+              position={Position.Right}
+              className='canvas-node-handle-base canvas-node-handle-right'
+            >
+            </Handle>
+            <Handle
+              type="source"
+              id="bottom"
+              position={Position.Bottom}
+              className='canvas-node-handle-base canvas-node-handle-bottom'
+            />
+            <Handle
+              type="target"
+              id="bottom"
+              position={Position.Bottom}
+              className='canvas-node-handle-base canvas-node-handle-bottom'
             />
           </div>
         </div>
