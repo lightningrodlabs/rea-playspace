e593d2552607b8df396dcfdf78f1c19fbcf04340
Jillian Ada Burrows <jill@adaburrows.com>
2022-09-23 01:04:58 -0700

    Turn EconomicResource form into a component.

--- ui/src/components/input/EconomicResource.tsx
+++ ui/src/components/input/EconomicResource.tsx
@@ -1,15 +1,14 @@
-import { SlButton, SlButtonGroup, SlDivider, SlInput, SlMenuItem, SlSelect, SlTextarea } from "@shoelace-style/shoelace/dist/react";
+import { SlInput, SlMenuItem, SlSelect, SlTextarea } from "@shoelace-style/shoelace/dist/react";
 import { useEffect, useState } from "react";
-import getDataStore from "../../data/DataStore";
-import { EconomicResource } from "../../data/models/Valueflows/Observation";
 import { AgentShape, EconomicResourceShape, ResourceSpecificationShape } from "../../types/valueflows";
 import { slChangeConstructor } from '../util';
 
 interface Props {
+  resourceState: EconomicResourceShape;
   conformingResource: ResourceSpecificationShape;
   agents: AgentShape[];
-  afterHandleResourceSubmit: (resource: string) => void;
-  resetResourceState: () => void;
+  name: string;
+  onChange?: (event: any) => void;
 }
 
 const initialState: EconomicResourceShape = {
@@ -28,44 +27,16 @@ const initialState: EconomicResourceShape = {
   lot: ''
 };
 
-const EconomicResourceInput: React.FC<Props> = ({ conformingResource, agents, afterHandleResourceSubmit, resetResourceState }) => {
+const EconomicResourceInput: React.FC<Props> = ({ resourceState, conformingResource, agents, name: fieldName, onChange }) => {
   const [
     {name, conformsTo, primaryAccountable, trackingIndentifier, currentLocation, note, classifiedAs, image, unitOfEffort, state, stage, containedIn, lot}, setState
   ] = useState({ ...initialState });
 
-  let resourceUpdates: EconomicResourceShape = null;
-  /**
-  * When the resource changes, update it.
-  */
-  const handleResourceChange = (e: any) => {
-    resourceUpdates = new EconomicResource(e.target.value);
-  }
-
   useEffect(()=>{
-    setState(prevState => ({ ...prevState, conformsTo: conformingResource.name }));
+    setState(prevState => ({ ...prevState, resourceState }));
   },[]);
 
-  const onSlChange = slChangeConstructor<EconomicResourceShape>(name, handleResourceChange, setState, null);
-
-  const handleResourceSubmit = () => {
-    // persist to DHT
-    const store = getDataStore();
-    const init: EconomicResourceShape = {
-      name,
-      conformsTo,
-      primaryAccountable,
-      trackingIndentifier,
-      currentLocation,
-      note,  
-      state,
-      stage
-    }
-    const resource = new EconomicResource(init);
-    store.set(resource);
-
-    // slide panel back to either commitment or event
-    afterHandleResourceSubmit(resource.id);
-  }
+  const onSlChange = slChangeConstructor<EconomicResourceShape>(fieldName, onChange, setState);
 
   return (
     <>
@@ -79,21 +50,16 @@ const EconomicResourceInput: React.FC<Props> = ({ conformingResource, agents, af
         {agents.map((agent) => (<SlMenuItem key={`provider_${agent.id}`} value={agent.id}>{agent.name}</SlMenuItem>))}
       </SlSelect>
       <br />
-      <SlInput label="Stage" name="stage" value={stage}></SlInput>
-      <br/ >
-      <SlInput label="State" name="state" value={state}></SlInput>
-      <br/ >
+      <SlInput disabled label="Stage" name="stage" value={stage}></SlInput>
+      <br />
+      <SlInput disabled label="State" name="state" value={state}></SlInput>
+      <br />
       <SlTextarea
         label='Note'
         name='note'
         // @ts-ignore
         value={note}
       ></SlTextarea>
-      <SlDivider></SlDivider>
-      <SlButtonGroup slot="footer">
-        <SlButton onClick={handleResourceSubmit} variant="primary">Create</SlButton>
-        <SlButton onClick={resetResourceState} variant="default">Cancel</SlButton>
-      </SlButtonGroup>
     </>
   );
 }
