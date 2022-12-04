31b898be998e24ab8c26a04c03d609013b6ee278
Jillian Ada Burrows <jill@adaburrows.com>
2022-12-04 16:00:01 -0500

    Fix #172: Allow creating and selecting plans.

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,20 +1,71 @@
-import React from "react";
-import FlowCanvas from "../components/layout/FlowCanvas";
-import Pallet from "../components/layout/Pallet";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import MainPanelHeader from "../components/layout/MainPanelHeader";
+import { useNavigate } from "react-router-dom";
+import { getDataStore } from "../data/DataStore";
+import { usePath } from "Yaati";
+import { Root } from "../data/models/Application/Root";
+import { Pathed } from "data-providers";
+import { Plan } from "valueflows-models";
+import Table from "../components/layout/Table";
 
-interface Props {
-  setEdit: (entity: any) => void;
-}
+export type HomeProps = {}
 
-const Home: React.FC<Props> = ({setEdit}) => {
-  return(
-    <div style={{display:"flex"}}>
-      <Pallet
-        setEdit={setEdit}
-      />
-      <FlowCanvas />
-    </div>
-  )
-}
+const Home: React.FC<HomeProps> = () => {
+  const store = getDataStore();
+  const dataRecords = usePath<'root', Root, Pathed<Plan>>('root.plan', store);
+  const [datas, setDatas] = useState<Array<Pathed<Plan>>>([]);
+
+  useEffect(()=>{
+    setDatas(Object.values(dataRecords));
+  },[dataRecords]);
+
+  const descriptors = {
+    'action': "Available Plans"
+  }
+  const synthetic = {
+    'action': (data: Pathed<Plan>) => [`/plans/edit/${data.id}`, data.name]
+  }
+  const decorators = {
+    'action': (data: any) => <><Link to={data[0]}>{data[1]}</Link></>
+  }
+
+  const RenderPlans= (): JSX.Element => {
+    if (datas.length === 0) {
+      return(
+        <>
+          <div>No Plans</div>
+        </>
+      );
+    } else {
+      return (
+        <Table
+          datas={datas}
+          fieldDescriptors={descriptors}
+          syntheticFields={synthetic}
+          fieldDecorators={decorators}>
+        </Table>
+      );
+    }
+  }
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <MainPanelHeader>
+        <h1>Welcome to the REA Playspace</h1>
+      </MainPanelHeader>
+      <p>
+        To get started, you should create some <Link to={"agents"}>Agents</Link>, <Link to={"resource-specifications"}>ResourceSpecifications</Link>.
+        After you have those basic necessities, make a <Link to={"plans"}>Plan</Link> or two.
+      </p>
+      <hr />
+      <p>
+        <RenderPlans />
+      </p>
+    </>
+  );
+};
 
 export default Home;
---
+++ ui/src/pages/create/PlanView.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import { SlButton, SlCard, SlInput, SlTextarea } from "@shoelace-style/shoelace/dist/react";
+import { Link, useParams } from "react-router-dom";
+import MainPanelHeader from "../../components/layout/MainPanelHeader";
+import { ExtendedPlan } from "../../data/models/Application";
+import { useNavigate } from "react-router-dom";
+import { getDataStore } from "../../data/DataStore";
+import { PathFunctor } from "data-providers";
+
+export type NewPlanProps = {}
+
+const initialState = {
+  name: '',
+  note: ''
+}
+
+const NewPlan: React.FC<NewPlanProps> = () => {
+  const [
+    {name, note}, setState
+  ] = useState(initialState);
+
+  const navigate = useNavigate();
+  let { id } = useParams();
+
+  const clearState = () => {
+    setState({ ...initialState });
+  };
+
+  useEffect(() => {
+    if (id) {
+      const store = getDataStore();
+      const obj = store.getById<ExtendedPlan>(id);
+      setState({
+        name: obj.name ? obj.name : '',
+        note: obj.note ? obj.note : ''
+      })
+    };
+  }, []);
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setState(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const store = getDataStore();
+
+    const rs =  new ExtendedPlan({id, name, note});
+    const pr = PathFunctor(rs, `root.plan.${rs.id}`);
+    store.upsert(pr, ExtendedPlan);
+
+    clearState();
+    navigate('/plans');
+  }
+
+  function header() {
+    if (id) {
+      return <>Edit Plan</>
+    } else {
+      return <>New Plan</>
+    }
+  }
+
+  return (
+    <>
+      <MainPanelHeader>
+        <h2>{header()}</h2>
+        <Link to="/plans">
+          <SlButton variant="warning">Cancel</SlButton>
+        </Link>
+      </MainPanelHeader>
+      <SlCard className="create-resource scrollable-view">
+      <form onSubmit={handleSubmit}>
+      <br />
+        <br />
+        <SlInput
+          required
+          label="Name"
+          name="name"
+          // @ts-ignore
+          onSlInput={onChange}
+          value={name}
+
+        />
+        <br />
+        <SlTextarea
+          label='Note'
+          name='note'
+          // @ts-ignore
+          onSlInput={onChange}
+          value={note}
+        />
+        <br />
+        <SlButton type="submit" variant="primary">
+          { id ? 'Update' : 'Create' }
+        </SlButton>
+      </form>
+    </SlCard>
+    </>
+  );
+};
+
+export default NewPlan;
+
+
